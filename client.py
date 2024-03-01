e654e99a69900817b7677ad5c7ae9e821041c40e
Ali Hassan <121449949+Alih111@users.noreply.github.com>
2024-03-01 10:58:04 +0200

    test cases done

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import socket
+import time
+
 header=256
-port=6000
+port=5111
 SERVER=socket.gethostbyname(socket.gethostname())
 ENCODING='utf-8'
-Disconnect_msg="good bye kambora"
+Disconnect_msg="good bye"
 ADDRESS=(SERVER,port)
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(ADDRESS)#client tries to connect to server
 def recieve():
-    msg_len=client.recv(16).decode(ENCODING)
+    msg_len=client.recv(64).decode(ENCODING)
     msg_len= int(msg_len)
     msg=client.recv(msg_len).decode(ENCODING)
     return msg
@@ -20,6 +22,11 @@ def send(string):
     client.send(msg)
     print(recieve())
 
-send('r999lawdddaa 8nshuu')
-
+send('Wpython Socket Server')
+send('LpythonSocketServer')
+send('UPYTHONSOCKETSERVER')
+send('R1234567890')
+send('TpythonSocketServer123')
+send('pythonSocketServer123')
 send(Disconnect_msg)
+client.close()
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socket
 import threading
 class myServer(socketserver.TCPServer):
     # construter parameters (server_address, RequestHandlerClass, bind_and_activate=True)
+    allow_reuse_address = True  # Allow the server to reuse the address
     def __init__(self,address,handler,active):
         super().__init__(address, handler, active)#This uses the internet TCP protocol, which provides for continuous streams of data between the client and server.
 
@@ -28,28 +29,28 @@ class reqHandler(socketserver.BaseRequestHandler):
                 elif string[0].lower() == 'r':#count number of numbers in a string
                     server_msg =str(sum(1 for char in my_string if char.isdigit()))
                 elif string[0].lower() == 't':#count number of characters in a string
+                    server_msg=str(len(my_string)+1)
+                elif string[0].upper() == 'U':#count the number upper case letters
+                    print(my_string)
+                    server_msg =str(sum(1 for char in my_string if char.isupper()))
 
-                    server_msg=len(my_string)
-                elif string[0].lower() == 'u':#count the number upper case letters
-                    str(sum(1 for char in my_string if char.isupper()))
                 else:
                     #the string didnot start with a specified letter
                     server_msg=str(string)
-                print(server_msg)
                 server_msg_length=len(server_msg)
-                server_msg_length_str = f"{server_msg_length: <{16}}".encode('utf-8')#send a 16 byte string to the client containg the length of the message
+                server_msg_length_str = f"{server_msg_length: <{64}}".encode('utf-8')#send a 16 byte string to the client containg the length of the message
 
                 self.request.sendall((server_msg_length_str))#here the server sends the answer to the client
                 self.request.sendall(server_msg.encode('utf-8'))
 
-                if string == "good bye kambora":
+                if server_msg=='good bye':
                     connected = False
 
         self.request.close()
 
 
 #create server
-port=6000
+port=5111
 serv=socket.gethostbyname(socket.gethostname())
 print(serv)
 ADDRESS=(serv,port)
